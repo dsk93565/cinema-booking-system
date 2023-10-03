64b8e56f62f574ae893a2f0b9bc59aa0ed5e1ab1
Danny Kim <dsk93565@uga.edu>
2023-10-02 20:42:25 -0400

    Added template for SearchResults page

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,6 +1,12 @@
+import '../stylings/search.css';
+
 const SearchResults = () => {
   return (
-    <div>SearchResults</div>
+    <section className='search-results section-wrapper'>
+      <div className='section-container-top'>
+        <h2>Search results</h2>
+      </div>
+    </section>
   )
 }
 
