0c6c7fc588292c7f336fcbf4c5392e6b134a57df
mossivy <hmossjvy@gmail.com>
2023-11-07 12:51:52 -0500

    created utils.py and put Save_Card in utils.py

---
+++ cinemabackend/server/utils.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+
+class Save_Card():
+    def saveCard(self, request):
+        key = Fernet.generate_key()
+        fern = Fernet(key)
+        user = request[0]
+        data = request[1]
+        cardType = data.get('cardType')
+        cardNumber = data.get('cardNumber')
+        expiration = data.get('expirationDate')
+        billingStreet = data.get('billingStreetAddress')
+        billingCity = data.get('billingCityAddress')
+        billingState = data.get('billingStateAddress)')
+        billingZip = data.get('billingZipCodeAddress')
+        if cardType is not None:
+            print("DATA TYPE:", type(cardType))
+            cardType = fern.encrypt(cardType.encode('utf-8'))
+        if cardNumber is not None:
+            cardNumber = fern.encrypt(cardNumber.encode('utf-8'))
+        if expiration is not None:
+            expiration = fern.encrypt(expiration.encode('utf-8'))
+        if billingStreet is not None:
+            billingStreet = fern.encrypt(billingStreet.encode('utf-8'))
+        if billingCity is not None:
+            billingCity = fern.encrypt(billingCity.encode('utf-8'))
+        if billingState is not None:
+            billingState = fern.encrypt(billingState.encode('utf-8'))
+        if billingZip is not None: 
+            billingZip = fern.encrypt(billingZip.encode('utf-8'))
+        new_card = Card.objects.create(user_id=user, card_type=cardType, card_number=cardNumber, 
+                                       card_expiration=expiration, card_city=billingCity, card_street=billingStreet, 
+                                       card_state=billingState, card_zip=billingZip)
+        new_card.save()
+        return Response(key)
--- cinemabackend/server/views.py
+++ cinemabackend/server/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .models import Movies, CustomUser, Card
-from .serializer import MovieSerializer, UserSerializer
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login
 from django.core.mail import send_mail
+from .models import Movies, CustomUser, Card
+from .serializer import MovieSerializer, UserSerializer
+from .utils import Save_Card
 import json
 import uuid
-from cryptography.fernet import Fernet
 
 #TODO: define error class and error codes and properly throw errors
 
@@ -24,41 +24,6 @@ class Send_Verification_Email(APIView):
         email = data.get('email')
         send_mail(subject, verification_code, 'cineraecinemabooking@gmail.com', email)
         return Response({'email sent': 1})
-
-
-class Save_Card():
-    def saveCard(self, request):
-        key = Fernet.generate_key()
-        fern = Fernet(key)
-        user = request[0]
-        data = request[1]
-        cardType = data.get('cardType')
-        cardNumber = data.get('cardNumber')
-        expiration = data.get('expirationDate')
-        billingStreet = data.get('billingStreetAddress')
-        billingCity = data.get('billingCityAddress')
-        billingState = data.get('billingStateAddress)')
-        billingZip = data.get('billingZipCodeAddress')
-        if cardType is not None:
-            print("DATA TYPE:", type(cardType))
-            cardType = fern.encrypt(cardType.encode('utf-8'))
-        if cardNumber is not None:
-            cardNumber = fern.encrypt(cardNumber.encode('utf-8'))
-        if expiration is not None:
-            expiration = fern.encrypt(expiration.encode('utf-8'))
-        if billingStreet is not None:
-            billingStreet = fern.encrypt(billingStreet.encode('utf-8'))
-        if billingCity is not None:
-            billingCity = fern.encrypt(billingCity.encode('utf-8'))
-        if billingState is not None:
-            billingState = fern.encrypt(billingState.encode('utf-8'))
-        if billingZip is not None: 
-            billingZip = fern.encrypt(billingZip.encode('utf-8'))
-        new_card = Card.objects.create(user_id=user, card_type=cardType, card_number=cardNumber, 
-                                       card_expiration=expiration, card_city=billingCity, card_street=billingStreet, 
-                                       card_state=billingState, card_zip=billingZip)
-        new_card.save()
-        return Response(key)
 
 #creates user and card in database
 class Create_User(APIView):
@@ -101,4 +66,4 @@ class MovieList(APIView):
         print(serializer_class.data)
         movieList = {"movies":serializer_class.data}
         return Response(movieList)
-# Create your views here.
+
