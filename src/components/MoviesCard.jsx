f8ba2a1d8a61322600a2e7ffa289a41554525a92
unw1018 <145299646+unw1018@users.noreply.github.com>
2023-12-03 13:53:22 -0500

    + stuff
    phone number and address

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import MovieCard from './MovieCard';
 
-
 const MoviesCard = (props) => {
     const [movies, setMovies] = useState([]);
+    const [flippedCard, setFlippedCard] = useState(null); // State to track flipped card
 
     useEffect(() => {
         fetchMovies(props.sectionTitle);
@@ -13,35 +12,29 @@ const MoviesCard = (props) => {
 
     const sectionRef = useRef(null);
 
-    const scrollSection = (dir) => {
-        if (sectionRef.current) {
-            const currentScrollLeft = sectionRef.current.scrollLeft;
-            const sectionWidth = sectionRef.current.offsetWidth;
-            
-            sectionRef.current.scrollTo({
-                left: currentScrollLeft + (sectionWidth * dir),
-                behavior: 'smooth', // You can use 'auto' for instant scrolling
-            });
-        }
-    };
-        
     const fetchMovies = async (sectionTitle, categoryFilter) => {
         let response;
-    
+
         try {
             response = await axios.get(`http://localhost:8000/api/get-movies`);
             const { data } = response;
-    
+
             if (sectionTitle === 'Trending' || sectionTitle === 'Now Playing' || sectionTitle === 'Coming Soon') {
-                // No filtering needed for these sections
                 setMovies(data.movies);
             } else {
-                // Filter the movies based on the category
                 const filteredMovies = data.movies.filter(movie => movie.category === categoryFilter);
                 setMovies(filteredMovies);
             }
         } catch (error) {
             console.error('Error fetching movies:', error);
+        }
+    };
+
+    const handleFlip = (movieId) => {
+        if (flippedCard === movieId) {
+            setFlippedCard(null);
+        } else {
+            setFlippedCard(movieId);
         }
     };
 
@@ -51,17 +44,27 @@ const MoviesCard = (props) => {
             <div className='movies-container' ref={sectionRef}>
                 {movies.map(movie => (
                     <div key={movie.mid}>
-                        <MovieCard movie={movie} origin="MoviesCard" />
+                        <MovieCard 
+                            movie={movie} 
+                            origin="MoviesCard" 
+                            isFlipped={flippedCard === movie.mid} 
+                            onFlip={handleFlip} />
                         <h3 className='movie-title'>{movie.title}</h3>
                     </div>
                 ))}
             </div>
             <div>
-                <button className='scroll-button' onClick={() => scrollSection(-1)}>❮</button>
-                <button className='scroll-button' onClick={() => scrollSection(1)}>❯</button>
+                <button className='scroll-button' onClick={() => sectionRef.current.scrollTo({
+                    left: sectionRef.current.scrollLeft - sectionRef.current.offsetWidth,
+                    behavior: 'smooth'
+                })}>❮</button>
+                <button className='scroll-button' onClick={() => sectionRef.current.scrollTo({
+                    left: sectionRef.current.scrollLeft + sectionRef.current.offsetWidth,
+                    behavior: 'smooth'
+                })}>❯</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default MoviesCard;
