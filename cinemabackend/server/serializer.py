c4d194ab6069d3e462bf8457779352ae257d82bd
mossivy <hmossjvy@gmail.com>
2023-12-06 16:00:12 -0500

    added state_id to movie serializer

--- cinemabackend/server/serializer.py
+++ cinemabackend/server/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movies, CustomUser, Card, Showings
+from .models import Movies, CustomUser, Card, Showings,Movie_States
 
 class UserSerializer(serializers.ModelSerializer):
         class Meta: 
@@ -12,8 +12,9 @@ class ShowingSerializer(serializers.ModelSerializer):
               model = Showings
               fields = ['shid', 'movie_id', 'period_id', 'room_id', 'room_id', 'show_date'] 
 class MovieSerializer(serializers.ModelSerializer):
+        state_id = serializers.CharField(source='state_id.movie_state', read_only=True)
         class Meta:
             model = Movies
             # this passes all the fields
-            fields = ['mid', 'category', 'cast', 'director', 'producer', 'synopsis', 'reviews', 'trailer', 'rating', 'title', 'poster_path']
+            fields = ['mid', 'category', 'cast', 'director', 'producer', 'synopsis', 'reviews', 'trailer', 'rating', 'title', 'poster_path', 'state_id']
             # to restrict the fields do fields = [value1, value2, ...]
