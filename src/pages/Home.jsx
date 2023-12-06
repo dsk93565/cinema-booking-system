e6ebade2fe9827a9ae1c3364778da2b7d2cd3d4c
Danny Kim <dsk93565@uga.edu>
2023-12-06 18:17:58 -0500

    Categorized movies in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Hero from '../components/Hero';
 import MoviesCard from '../components/MoviesCard';
 import Footer from '../components/Footer';
@@ -5,22 +6,32 @@ import '../stylings/home.css';
 
 
 const Home = () => {
+  const [flippedCard, setFlippedCard] = useState(null);
+
+  const handleFlip = (movieId) => {
+    if (flippedCard === movieId) {
+      setFlippedCard(null);
+    } else {
+      setFlippedCard(movieId);
+    }
+  };
+
   return (
     <div className='home'>
       <Hero />
       <section className='section-wrapper'> {/* Trending */}
         <div className='section-container'>
-        <MoviesCard sectionTitle='Trending' />
+          <MoviesCard sectionTitle='Trending' flippedCard={flippedCard} onFlip={handleFlip} />
         </div>
       </section>
       <section className='section-wrapper'> {/* Now Playing */}
         <div className='section-container'>
-          <MoviesCard sectionTitle='Now Playing' />
+          <MoviesCard sectionTitle='Now Playing' flippedCard={flippedCard} onFlip={handleFlip} />
         </div>
       </section>
       <section className='section-wrapper'> {/* Coming Soon */}
         <div className='section-container'>
-          <MoviesCard sectionTitle='Coming Soon' />
+          <MoviesCard sectionTitle='Coming Soon' flippedCard={flippedCard} onFlip={handleFlip} />
         </div>
       </section>
       <section className='promo-subscription section-wrapper'>
