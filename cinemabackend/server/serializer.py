308393bf7ba72f5442a93421e7350a4361ab8281
mossivy <hmossjvy@gmail.com>
2023-12-09 15:20:29 -0500

    added release date to movie serializer

--- cinemabackend/server/serializer.py
+++ cinemabackend/server/serializer.py
@@ -47,5 +47,5 @@ class MovieSerializer(serializers.ModelSerializer):
         class Meta:
             model = Movies
             # this passes all the fields
-            fields = ['mid', 'category', 'cast', 'director', 'producer', 'synopsis', 'reviews', 'trailer', 'rating', 'title', 'poster_path', 'state_id']
+            fields = ['mid', 'category', 'release_date', 'cast', 'director', 'producer', 'synopsis', 'reviews', 'trailer', 'rating', 'title', 'poster_path', 'state_id']
             # to restrict the fields do fields = [value1, value2, ...]
